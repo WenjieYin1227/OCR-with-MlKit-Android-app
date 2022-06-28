001a2b68fa62c387ca734100513a1de2e99ce69c
WenjieYin1227 <96346074+WenjieYin1227@users.noreply.github.com>
2022-06-29 00:46:26 +0900

    second commit

---
+++ OCRWithMlKit/MainActivity.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using AndroidX.AppCompat.Widget;
+using AndroidX.AppCompat.App;
+using Google.Android.Material.FloatingActionButton;
+using Google.Android.Material.Snackbar;
+using AndroidX.Camera.Core;
+using Java.Util.Concurrent;
+using AndroidX.Camera.View;
+using Android;
+using System.Linq;
+using AndroidX.Core.Content;
+using AndroidX.Core.App;
+using Android.Widget;
+using AndroidX.Camera.Lifecycle;
+using Java.Lang;
+using Android.Util;
+
+namespace OCRWithMlKit
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+
+        private const string TAG = "OCR Log: ";
+        private const int REQUEST_CODE_PERMISSIONS = 10;
+
+        ImageCapture imageCapture;
+        IExecutorService cameraExecutor;
+
+        PreviewView viewFinder;
+
+        TextView OutputTextView;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            viewFinder = FindViewById<PreviewView>(Resource.Id.viewFinder);
+            OutputTextView = FindViewById<TextView>(Resource.Id.textView1);
+
+            // Request camera permissions   
+            string[] permissions = new string[] { Manifest.Permission.Camera};
+            if (permissions.FirstOrDefault(x => ContextCompat.CheckSelfPermission(this, x) != Android.Content.PM.Permission.Granted) != null) //   ContextCompat.CheckSelfPermission(this, Manifest.Permission.Camera) == Android.Content.PM.Permission.Granted)
+                ActivityCompat.RequestPermissions(this, permissions, REQUEST_CODE_PERMISSIONS);
+            else
+                StartCamera();
+
+            cameraExecutor = Executors.NewSingleThreadExecutor();
+
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            if (requestCode == REQUEST_CODE_PERMISSIONS)
+            {
+                if (permissions.FirstOrDefault(x => ContextCompat.CheckSelfPermission(this, x) != Android.Content.PM.Permission.Granted) == null)
+                {
+                    StartCamera();
+                }
+                else
+                {
+                    Toast.MakeText(this, "Permissions not granted by the user.", ToastLength.Short).Show();
+                    this.Finish();
+                    return;
+                }
+            }
+        }
+
+        private void StartCamera()
+        {
+            var cameraProviderFuture = ProcessCameraProvider.GetInstance(this);
+
+            cameraProviderFuture.AddListener(new Runnable(() =>
+            {
+                // Used to bind the lifecycle of cameras to the lifecycle owner
+                var cameraProvider = (ProcessCameraProvider)cameraProviderFuture.Get();
+
+                // Preview
+                var preview = new Preview.Builder().Build();
+                preview.SetSurfaceProvider(viewFinder.CreateSurfaceProvider());
+
+                // Take Photo
+                this.imageCapture = new ImageCapture.Builder().Build();
+
+                // Frame by frame analyze
+                var imageAnalyzer = new ImageAnalysis.Builder().Build();
+                imageAnalyzer.SetAnalyzer(cameraExecutor, new TextAnalyzer(SetNewText));
+
+                // Select back camera as a default, or front camera otherwise
+                CameraSelector cameraSelector = null;
+                if (cameraProvider.HasCamera(CameraSelector.DefaultBackCamera) == true)
+                    cameraSelector = CameraSelector.DefaultBackCamera;
+                else if (cameraProvider.HasCamera(CameraSelector.DefaultFrontCamera) == true)
+                    cameraSelector = CameraSelector.DefaultFrontCamera;
+                else
+                    throw new System.Exception("Camera not found");
+
+                try
+                {
+                    // Unbind use cases before rebinding
+                    cameraProvider.UnbindAll();
+
+                    // Bind use cases to camera
+                    cameraProvider.BindToLifecycle(this, cameraSelector, preview, imageCapture, imageAnalyzer);
+                }
+                catch (Java.Lang.Exception exc)
+                {
+                    Log.Debug(TAG, "Use case binding failed", exc);
+                    Toast.MakeText(this, $"Use case binding failed: {exc.Message}", ToastLength.Short).Show();
+                }
+
+            }), ContextCompat.GetMainExecutor(this)); //GetMainExecutor: returns an Executor that runs on the main thread.
+        }
+
+        void SetNewText(string text)
+        {
+            RunOnUiThread(() =>
+            {
+                if (!string.IsNullOrEmpty(text))
+                    OutputTextView.Text = text;
+                else
+                    OutputTextView.Text = "No Text Found";
+            });
+        }
+
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+            cameraExecutor.Shutdown();
+        }
+    }
+}
---
+++ OCRWithMlKit/TextAnalyzer.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Android.Gms.Tasks;
+using Android.Graphics;
+using Android.Media;
+using Android.Util;
+using AndroidX.Camera.Core;
+using Java.Nio;
+using Xamarin.Google.MLKit.Vision.Common;
+using Xamarin.Google.MLKit.Vision.Text;
+
+namespace OCRWithMlKit
+{
+    public class TextAnalyzer : Java.Lang.Object, ImageAnalysis.IAnalyzer
+    {
+        private const string TAG = "OCR Log: ";
+        private readonly Action<string> TextListerner;
+
+        public TextAnalyzer(Action<string> callback) //LumaListener listener)
+        {
+            this.TextListerner = callback;
+        }
+
+        public async void Analyze(IImageProxy image)
+        {
+            //var buffer = image.GetPlanes()[0].Buffer;
+            //var data = ToByteArray(buffer);
+
+            //var pixels = data.ToList();
+            //pixels.ForEach(x => x = (byte)((int)x & 0xFF));
+            //var luma = pixels.Average(x => x);
+            ////Log.Debug(TAG, $"Average luminosity: {luma}");
+
+            //
+
+            var Rotation = image.ImageInfo.RotationDegrees;
+            Log.Error("OCR Log", "Rotation : " + Rotation);
+            var bitmap = yuv420ToBitmap(image.Image);
+
+
+            Matrix matrix = new Matrix();
+            matrix.PostRotate(Rotation);
+            bitmap = Bitmap.CreateBitmap(bitmap, 0, 0, bitmap.Width, bitmap.Height, matrix, true);
+
+            string output = await ExtractText(bitmap);
+
+            image.Close();
+            TextListerner.Invoke(output);
+
+        }
+
+        public static Task<Java.Lang.Object> ToAwaitableTask(Android.Gms.Tasks.Task task)
+        {
+            var taskCompletionSource = new TaskCompletionSource<Java.Lang.Object>();
+            var taskCompleteListener = new TaskCompleteListener(taskCompletionSource);
+            task.AddOnCompleteListener(taskCompleteListener);
+
+            return taskCompletionSource.Task;
+        }
+
+        public async Task<string> ExtractText(Bitmap bitmap)
+        {
+            //System.IO.Stream stream = new MemoryStream(data);
+            //var bitmap = BitmapFactory.DecodeStream(stream);
+            //var base64 = Convert.ToBase64String(data);
+            var inputImage = InputImage.FromBitmap(bitmap, 0);
+
+            var b = new TextRecognizerOptions.Builder();
+            var opts = b.Build();
+            var recognizer = TextRecognition.GetClient(opts);
+            var t = (Text)await ToAwaitableTask(recognizer.Process(inputImage));
+
+            var output = "";
+
+            foreach (var item in t.TextBlocks)
+            {
+                output += item.Text + " \n";
+            }
+
+            return output;
+        }
+
+        Bitmap yuv420ToBitmap(Image image)
+        {
+            int imageWidth = image.Width;
+            int imageHeight = image.Height;
+            // sRGB array needed by Bitmap static factory method I use below.
+            int[] argbArray = new int[imageWidth * imageHeight];
+            ByteBuffer yBuffer = image.GetPlanes()[0].Buffer;
+            yBuffer.Position(0);
+
+            // This is specific to YUV420SP format where U & V planes are interleaved
+            // so you can access them directly from one ByteBuffer. The data is saved as
+            // UVUVUVUVU... for NV12 format and VUVUVUVUV... for NV21 format.
+            //
+            // The alternative way to handle this would be refer U & V as separate
+            // `ByteBuffer`s and then use PixelStride and RowStride to find the right
+            // index of the U or V value per pixel.
+            ByteBuffer uvBuffer = image.GetPlanes()[1].Buffer;
+            uvBuffer.Position(0);
+            int r, g, b;
+            int yValue, uValue, vValue;
+
+            for (int y = 0; y < imageHeight - 2; y++)
+            {
+                for (int x = 0; x < imageWidth - 2; x++)
+                {
+                    int yIndex = y * imageWidth + x;
+                    // Y plane should have positive values belonging to [0...255]
+                    yValue = (yBuffer.Get(yIndex) & 0xff);
+
+                    int uvx = x / 2;
+                    int uvy = y / 2;
+                    // Remember UV values are common for four pixel values.
+                    // So the actual formula if U & V were in separate plane would be:
+                    // `pos (for u or v) = (y / 2) * (width / 2) + (x / 2)`
+                    // But since they are in single plane interleaved the position becomes:
+                    // `u = 2 * pos`
+                    // `v = 2 * pos + 1`, if the image is in NV12 format, else reverse.
+                    int uIndex = uvy * imageWidth + 2 * uvx;
+                    // ^ Note that here `uvy = y / 2` and `uvx = x / 2`
+                    int vIndex = uIndex + 1;
+
+                    uValue = (uvBuffer.Get(uIndex) & 0xff) - 128;
+                    vValue = (uvBuffer.Get(vIndex) & 0xff) - 128;
+                    r = (int)(yValue + 1.370705f * vValue);
+                    g = (int)(yValue - (0.698001f * vValue) - (0.337633f * uValue));
+                    b = (int)(yValue + 1.732446f * uValue);
+                    r = clamp(r, 0, 255);
+                    g = clamp(g, 0, 255);
+                    b = clamp(b, 0, 255);
+                    // Use 255 for alpha value, no transparency. ARGB values are
+                    // positioned in each byte of a single 4 byte integer
+                    // [AAAAAAAARRRRRRRRGGGGGGGGBBBBBBBB]
+                    argbArray[yIndex] = (255 << 24) | (r & 255) << 16 | (g & 255) << 8 | (b & 255);
+                }
+            }
+
+            return Bitmap.CreateBitmap(argbArray, imageWidth, imageHeight, Bitmap.Config.Argb8888);
+        }
+
+        public int clamp(int val, int min, int max)
+        {
+            return Math.Max(min, Math.Min(max, val));
+        }
+
+        class TaskCompleteListener : Java.Lang.Object, IOnCompleteListener
+        {
+            private readonly TaskCompletionSource<Java.Lang.Object> taskCompletionSource;
+
+            public TaskCompleteListener(TaskCompletionSource<Java.Lang.Object> tcs)
+            {
+                this.taskCompletionSource = tcs;
+            }
+
+            public void OnComplete(Android.Gms.Tasks.Task task)
+            {
+                if (task.IsCanceled)
+                {
+                    this.taskCompletionSource.SetCanceled();
+                }
+                else if (task.IsSuccessful)
+                {
+                    this.taskCompletionSource.SetResult(task.Result);
+                }
+                else
+                {
+                    this.taskCompletionSource.SetException(task.Exception);
+                }
+            }
+        }
+
+    }
+}
